6e39e65ca51d113737f58fc2ead97b7f2ab963ab
Pablo Rafael <prfoose@gmail.com>
2020-07-24 18:05:41 -0300

    email
    scrips email

---
+++ js/scriptemail.js
@@ -0,0 +1,24 @@
+$(function($){
+	event.preventDefaut();
+
+	$.ajax({
+	url:"https://formspree.io/prfoose@gmail.com",
+	method: "POST",
+	data: {
+		name: $("#name").val(),
+		email: $("#email").val(),
+		about: $("#about").val(),
+		message: $("#message").val(),
+	},
+	dataType: "json"
+	}).done(function(){
+		$("#name").val("");
+		$("#email").val("");
+		$("#about").val(""),
+		$("#message").val("");
+		alert("Email enviado com sucesso!");
+	}).fail(function(){
+		alert("Erro ao enviar email!");
+	});
+});
+}) ;
